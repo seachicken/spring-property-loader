5039c9acc0e9b699a41b276a6d7989665d9842ef
seito.tanaka <seito.tanaka.dev@gmail.com>
2023-08-27 20:47:31 +0900

    Auto-find proferty files

--- src/main/java/inga/springpropertyloader/Main.java
+++ src/main/java/inga/springpropertyloader/Main.java
@@ -4,18 +4,28 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.nio.file.Path;
-import java.util.Scanner;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Main {
+    private static final Map<Path, List<Map<String, Object>>> propertyPathCache = new HashMap<>();
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         var mapper = new ObjectMapper();
 
         while (scanner.hasNextLine()) {
-            String path = scanner.nextLine();
-            var properties = PropertyLoader
-                    .findLoader(Path.of(path))
-                    .getProperties();
+            Path path = Path.of(scanner.nextLine());
+            List<Map<String, Object>> properties;
+            if (propertyPathCache.containsKey(path)) {
+                properties = propertyPathCache.get(path);
+            } else {
+                properties = PropertyLoader.findPropertyPaths(path)
+                        .stream()
+                        .flatMap(p -> PropertyLoader.findLoader(p).getProperties().stream())
+                        .collect(Collectors.toList());
+                propertyPathCache.put(path, properties);
+            }
             try {
                 var json = mapper.writeValueAsString(properties);
                 System.out.println(json);
---
+++ src/test/java/inga/springpropertyloader/PropertyLoaderTest.java
@@ -0,0 +1,44 @@
+package inga.springpropertyloader;
+
+import org.junit.jupiter.api.Test;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class PropertyLoaderTest {
+    @Test
+    void findPropertyPath() {
+        var actual = PropertyLoader.findPropertyPaths(
+                getFixturesPath("spring-tutorials/spring-boot-modules/spring-boot-properties/src/main/java/com/baeldung/buildproperties/Application.java")
+        );
+        assertThat(actual.stream().map(p -> Paths.get(System.getProperty("user.dir")).relativize(p)))
+                .containsExactly(
+                        Path.of("build/resources/test/fixtures/spring-tutorials/spring-boot-modules/spring-boot-properties/src/test/resources/additional-application-properties/application.properties"),
+                        Path.of("build/resources/test/fixtures/spring-tutorials/spring-boot-modules/spring-boot-properties/src/test/resources/conversion.properties"),
+                        Path.of("build/resources/test/fixtures/spring-tutorials/spring-boot-modules/spring-boot-properties/src/test/resources/configprops-test.properties"),
+                        Path.of("build/resources/test/fixtures/spring-tutorials/spring-boot-modules/spring-boot-properties/src/test/resources/foo.properties"),
+                        Path.of("build/resources/test/fixtures/spring-tutorials/spring-boot-modules/spring-boot-properties/src/test/resources/additional-application2.properties"),
+                        Path.of("build/resources/test/fixtures/spring-tutorials/spring-boot-modules/spring-boot-properties/src/test/resources/database-test.properties"),
+                        Path.of("build/resources/test/fixtures/spring-tutorials/spring-boot-modules/spring-boot-properties/src/test/resources/additional-application.properties"),
+                        Path.of("build/resources/test/fixtures/spring-tutorials/spring-boot-modules/spring-boot-properties/src/test/resources/application.properties"),
+                        Path.of("build/resources/test/fixtures/spring-tutorials/spring-boot-modules/spring-boot-properties/src/main/resources/build.properties"),
+                        Path.of("build/resources/test/fixtures/spring-tutorials/spring-boot-modules/spring-boot-properties/src/main/resources/configprops.properties"),
+                        Path.of("build/resources/test/fixtures/spring-tutorials/spring-boot-modules/spring-boot-properties/src/main/resources/conversion.properties"),
+                        Path.of("build/resources/test/fixtures/spring-tutorials/spring-boot-modules/spring-boot-properties/src/main/resources/bar.properties"),
+                        Path.of("build/resources/test/fixtures/spring-tutorials/spring-boot-modules/spring-boot-properties/src/main/resources/foo.properties"),
+                        Path.of("build/resources/test/fixtures/spring-tutorials/spring-boot-modules/spring-boot-properties/src/main/resources/database.yml"),
+                        Path.of("build/resources/test/fixtures/spring-tutorials/spring-boot-modules/spring-boot-properties/src/main/resources/child.properties"),
+                        Path.of("build/resources/test/fixtures/spring-tutorials/spring-boot-modules/spring-boot-properties/src/main/resources/parent.properties"),
+                        Path.of("build/resources/test/fixtures/spring-tutorials/spring-boot-modules/spring-boot-properties/src/main/resources/application.yml"),
+                        Path.of("build/resources/test/fixtures/spring-tutorials/spring-boot-modules/spring-boot-properties/src/main/resources/application.properties"),
+                        Path.of("build/resources/test/fixtures/spring-tutorials/spring-boot-modules/spring-boot-properties/src/main/resources/build.yml"),
+                        Path.of("build/resources/test/fixtures/spring-tutorials/spring-boot-modules/spring-boot-properties/src/main/resources/database.properties")
+                );
+    }
+
+    private Path getFixturesPath(String path) {
+        return Path.of(getClass().getClassLoader().getResource("fixtures/" + path).getFile());
+    }
+}
