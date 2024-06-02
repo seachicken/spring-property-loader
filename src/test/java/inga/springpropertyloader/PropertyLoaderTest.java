37008fb8e33717a3a1939874396c05a7cd775258
seito.tanaka <seito.tanaka.dev@gmail.com>
2024-06-02 20:44:35 +0900

    Fix tests

--- src/test/java/inga/springpropertyloader/PropertyLoaderTest.java
+++ src/test/java/inga/springpropertyloader/PropertyLoaderTest.java
@@ -16,7 +16,7 @@ class PropertyLoaderTest {
                 List.of("prod")
         );
         assertThat(actual.stream().map(p -> Paths.get(System.getProperty("user.dir")).relativize(p)))
-                .containsExactly(
+                .containsExactlyInAnyOrder(
                         Path.of("src/test/resources/fixtures/spring-tutorials/spring-boot-modules/spring-boot-properties/src/test/resources/additional-application-properties/application.properties"),
                         Path.of("src/test/resources/fixtures/spring-tutorials/spring-boot-modules/spring-boot-properties/src/test/resources/application.properties"),
                         Path.of("src/test/resources/fixtures/spring-tutorials/spring-boot-modules/spring-boot-properties/src/main/resources/application.yml"),
